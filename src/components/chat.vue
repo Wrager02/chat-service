75ab7c7735800865d8d54d89563d9a70956e4eef
Wrager02 <alre0209@gmail.com>
2020-10-31 19:02:42 +0100

    Add PubNub

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,24 +1,50 @@
 <template>
   <div id="chat">
     <h1>{{chat.name}}</h1>
+    <div id="messages">
+      <message v-for="message in chat.messages" :key="message.timestamp" :message="message"/>
+    </div>
+    <input type="text" v-model="message" @keypress.enter="publish" placeholder="Nachricht">
+    <button v-on:click="publish">Senden!</button>
   </div>
 </template>
 
 <script>
+import Message from "@/components/message";
 export default {
   name: "chat",
+  components: {Message},
   props: {
     type: String,
     url: String
   },
+  data() {
+    return {
+      message: "",
+    };
+  },
   computed: {
     chat() {
       return this.$store.getters.getChannel(this.url, this.type);
+    }
+  },
+  methods: {
+    publish() {
+      this.$store.commit('publish', {
+        message: this.message,
+        channel: this.chat.uuid,
+        chat: this.type,
+        group: this.url
+      });
+      this.message = "";
     }
   }
 }
 </script>
 
 <style scoped>
-
+#messages {
+  display: flex;
+  flex-direction: column;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import PubNub from 'pubnub'
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
@@ -14,49 +15,93 @@ import Settings from './components/settings'
 
 Vue.config.productionTip = false
 
+function getUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function getPublishKey() {
+  return '';
+}
+
+function getSubscribeKey() {
+  return '';
+}
+
+function getUsername() {
+  return "Alex";
+}
+
+function getGroups() {
+  return {
+    "gruppe-1": {
+      name: "Gruppe 1",
+      url: "gruppe-1",
+      channels: {
+        "allgemein": {
+          name: "Allgemein",
+          url: "allgemein",
+          uuid: "2532ef1c-4c1b-4a8a-8975-43149bca27e9",
+          messages: []
+        },
+        "wichtig": {
+          name: "Wichtig",
+          url: "wichtig",
+          uuid: "091c147f-c2ca-4232-ade9-3c9fb242d2aa",
+          messages: []
+        }
+      }
+    },
+    "gruppe-2": {
+      name: "Gruppe 2",
+      url: "gruppe-2",
+      channels: {
+        "allgemein": {
+          name: "Allgemein",
+          url: "allgemein",
+          uuid: "78ac78a6-54bd-46b2-8b4c-be073717e94c",
+          messages: []
+        },
+        "wichtig": {
+          name: "Wichtig",
+          url: "wichtig",
+          uuid: "6c8716bb-84c2-47e5-83fd-d0c19fa61189",
+          messages: []
+        }
+      }
+    },
+  };
+}
+
 const store = new Vuex.Store({
   state: {
-    groups: {
-      "gruppe-1": {
-        name: "Gruppe 1",
-        url: "gruppe-1",
-        channels: {
-          "allgemein": {
-            name: "Allgemein",
-            url: "allgemein",
-            uuid: "2532ef1c-4c1b-4a8a-8975-43149bca27e9",
-            messages: []
-          },
-          "wichtig": {
-            name: "Wichtig",
-            url: "wichtig",
-            uuid: "091c147f-c2ca-4232-ade9-3c9fb242d2aa",
-            messages: []
-          }
-        }
-      },
-      "gruppe-2": {
-        name: "Gruppe 2",
-        url: "gruppe-2",
-        channels: {
-          "allgemein": {
-            name: "Allgemein",
-            url: "allgemein",
-            uuid: "78ac78a6-54bd-46b2-8b4c-be073717e94c",
-            messages: []
-          },
-          "wichtig": {
-            name: "Wichtig",
-            url: "wichtig",
-            uuid: "6c8716bb-84c2-47e5-83fd-d0c19fa61189",
-            messages: []
-          }
-        }
-      },
-    }
+    pubnub: new PubNub({
+      publishKey: getPublishKey(),
+      subscribeKey: getSubscribeKey(),
+      uuid: getUUID()
+    }),
+    user: getUsername(),
+    groups: getGroups()
   },
   mutations: {
-
+    publish(state, payload) {
+      state.pubnub.publish(
+          {
+            channel: payload.channel,
+            message: {
+              'text': payload.message,
+              'user': state.user,
+              'group': payload.group,
+              'chat': payload.chat
+            }
+          }
+      );
+    },
+    addMessage(state, payload) {
+      state.groups[payload.message.message.group].channels[payload.message.message.chat].messages.push(payload.message);
+    }
   },
   getters: {
     getGroups: state => {
@@ -69,12 +114,32 @@ const store = new Vuex.Store({
       return state.groups[group].channels;
     },
     getChannel: state => (group, channel) => {
-      console.log("Group: " + group);
-      console.log("Channel: " + channel);
       return state.groups[group].channels[channel];
+    },
+    getAllChannelUuids: state => {
+      let r = [];
+
+      Object.keys(state.groups).forEach(groupKey => {
+        Object.keys(state.groups[groupKey].channels).forEach(channelKey => {
+          r.push(state.groups[groupKey].channels[channelKey].uuid);
+        });
+      });
+
+      return r;
     }
   }
-})
+});
+
+store.state.pubnub.addListener({
+  message: function(event) {
+    store.commit('addMessage', {message: event});
+  }
+});
+
+store.state.pubnub.subscribe({
+  channels: store.getters.getAllChannelUuids,
+  withPresence: true
+});
 
 const routes = [
   {
